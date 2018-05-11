0938a53048f0bbcd2840cd9f9cd22d71cc1033bb
Felipe Graeff <felipegraeff@hotmail.com>
2018-05-11 15:11:53 -0300

    Main class

---
+++ src/genius/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Genius
+ * Final project of the course Técnicas de Construção de Programas
+ * Universidade Federal do Rio Grande do Sul
+ *
+ * Created on : May 11, 2018
+ * Author     : Graeff
+ *
+ */
+package genius;
+
+/**
+ * @author Graeff
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
