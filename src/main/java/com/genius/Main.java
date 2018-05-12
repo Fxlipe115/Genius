61135641ef809c4c5ec96fa4ba7c1ce7548c5590
Felipe Graeff <Fxlipe115@users.noreply.github.com>
2018-05-11 21:43:01 -0300

    created main class

---
+++ src/main/java/com/genius/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Final project for the course Técnicas de Construção de Programas
+ * Universidade Federal do Rio Grande do Sul
+ * Instituto de Informática
+ *
+ * Created : May 11, 2018
+ * Author  : Graeff
+ */
+package com.genius;
+
+/**
+ * @author Graeff
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
