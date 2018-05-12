8934675439c87491c51f1fd9b67f3549fd51768f
Felipe Graeff <Fxlipe115@users.noreply.github.com>
2018-05-11 21:34:27 -0300

    Update LibraryTest.java

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,6 +8,5 @@ public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-        assertTrue(false);
     }
 }
