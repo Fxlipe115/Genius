2f4ac679878ce980ddd44456b73f78ebb16c7853
matheusrhoden <matheusrhoden@gmail.com>
2018-05-19 16:47:08 -0300

    created Player model class

--- src/main/java/genius/model/Player.java
+++ src/main/java/genius/model/Player.java
@@ -14,23 +14,30 @@ package genius.model;
  *
  */
 public class Player {
-	/**
-	 * 
-	 */
+	
 	private String name;
-
-	/**
-	 * 
-	 */
 	private int score;
-
-
-	/**
-	 * 
-	 */
-	public Player(){
-		super();
+	
+	public Player(String name, int score) {
+		this.name = name;
+		this.score = score;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}	
+	
 }
 
