b51cfa7cf25a7e6063fa2cd5c1e3b756bd6efc44
Felipe Graeff <felipegraeff@hotmail.com>
2018-06-04 23:10:20 -0300

    Score view done
    Scores being saved and loaded from file
    New scores can be added
    Can clear all scores
    For the future: will probably need some sort of back button for exiting

---
+++ src/main/java/genius/model/ScoresTableModel.java
@@ -0,0 +1,71 @@
+package genius.model;
+
+import javax.swing.table.AbstractTableModel;
+
+@SuppressWarnings("serial")
+public class ScoresTableModel extends AbstractTableModel {
+	private Scores scores;
+	private static final String[] HEADER = {"#", "Name", "Score"};
+
+	public ScoresTableModel() {
+		super();
+	}
+	
+	public ScoresTableModel(Scores scores) {
+		super();
+		
+		this.scores = scores;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return HEADER[column];
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		Object value = null;
+		switch (col) {
+		case 0:
+			value = row + 1;
+			break;
+		case 1:
+			value = scores.getScores().get(row).getName();
+			break;
+		case 2:
+			value = scores.getScores().get(row).getScore();
+		default:
+			break;
+		}
+		return value;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return HEADER.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		if(scores != null) {
+			return scores.getScores().size();
+		} else{
+			return 0;
+		}
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return false;
+	}
+
+	public Scores getScores() {
+		return scores;
+	}
+
+	public void setScores(Scores scores) {
+		this.scores = scores;
+		this.fireTableDataChanged();
+	}
+	
+}
--- src/main/java/genius/view/ScoresDialog.java
+++ src/main/java/genius/view/ScoresDialog.java
@@ -4,21 +4,61 @@
  * Instituto de Informática
  *
  * Created : May 13, 2018
- * Author  : 
+ * Author  : Graeff
  */
 package genius.view;
 
-import javax.swing.JDialog;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import javax.swing.border.BevelBorder;
+import javax.swing.ListSelectionModel;
 
 @SuppressWarnings("serial")
-public class ScoresDialog extends JDialog {
-
-	/**
-	 * Create the dialog.
-	 */
+public class ScoresDialog extends JPanel implements Observer{
+	private JTable table;
+	private JButton btnClearScores;
+	
 	public ScoresDialog() {
-		setBounds(100, 100, 450, 300);
+		setLayout(new BorderLayout(0, 0));
+		
+		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setRowSelectionAllowed(false);
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		add(new JScrollPane(table), BorderLayout.CENTER);
+		
+		btnClearScores = new JButton("Clear scores");
+		add(btnClearScores, BorderLayout.SOUTH);
+		
 
+	}
+	
+	
+	public void addController(ActionListener controller){
+		btnClearScores.addActionListener(controller);
+	}
+	
+	
+
+	public JTable getTable() {
+		return table;
+	}
+
+
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
 	}
 
 }
