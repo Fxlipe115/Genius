c2851dc37c900272ef4a74a0b553c34aa16b1795
Felipe Graeff <felipegraeff@hotmail.com>
2018-06-21 23:06:29 -0300

    Implemented main menu and application controller
    Still requires bugfixing

--- src/main/java/genius/controller/ApplicationController.java
+++ src/main/java/genius/controller/ApplicationController.java
@@ -4,73 +4,80 @@
  * Instituto de Informática
  *
  * Created : May 11, 2018
- * Author  : 
+ * Author  : Graeff 
  */
 package genius.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import genius.view.MainWindow;
+import genius.view.MenuPanel;
 
 /**
  * @author Graeff
  *
  */
-public class ApplicationController {
-	private MainWindow mainWindow; 
-	
-	/**
-	 * 
-	 */
-	private GameController gameController;
-	
-	/**
-	 * 
-	 */
-	private SettingsController settingsController;
-	
-	/**
-	 * 
-	 */
-	private ScoresController scoresController;
+public class ApplicationController implements ActionListener {
+	private MainWindow mainWindow;
 
-	
-	/**
-	 * 
-	 */
-	public ApplicationController(){
-		
-	}
-
-	
-	/**
-	 * 
-	 */
 	public void initApplication() {
 		mainWindow = new MainWindow();
+		openMenu();
 	}
 
-	
-	/**
-	 * 
-	 */
-	public void initGame() {
-		// TODO implement me
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "New Game":
+			initGame();
+			break;
+		case "Scores":
+			openScores();
+			break;
+		case "Options":
+			openSettings();
+			break;
+		case "Exit":
+			close();
+			break;
+		case "Back":
+			openMenu();
+			break;
+		default:
+			break;
+		}
 	}
 
-	
-	/**
-	 * 
-	 */
-	public void openSettings() {
-		// TODO implement me
+	private void openMenu() {
+		MenuPanel menuPanel = new MenuPanel();
+		menuPanel.addNewGameButtonListener(this);
+		menuPanel.addScoresButtonListener(this);
+		menuPanel.addOptionsButtonListener(this);
+		menuPanel.addExitButtonListener(this);
+		mainWindow.setContentPane(menuPanel);
 	}
 
-	
-	/**
-	 * 
-	 */
-	public void openScores() {
-		// TODO implement me
+	private void initGame() {
+		GameController gameController = new GameController();
+		gameController.getGameView().addController(this);
+		mainWindow.setContentPane(gameController.getGameView());
+	}
+
+	private void openSettings() {
+		SettingsController settingsController = new SettingsController();
+		settingsController.getSettingsView().addBackButtonController(this);
+		mainWindow.setContentPane(settingsController.getSettingsView());
+	}
+
+	private void openScores() {
+		ScoresController scoresController = new ScoresController();
+		scoresController.getScoresView().addBackButtonListener(this);
+		mainWindow.setContentPane(scoresController.getScoresView());
+	}
+
+	private void close() {
+		mainWindow.dispose();
 	}
 
 }
-
