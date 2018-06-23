58e0910f43bef472f8e0cfdc35c93c0ffa97d88a
matheusrhoden <matheusrhoden@gmail.com>
2018-06-22 22:47:01 -0300

    fixed null test

--- src/main/java/genius/controller/factory/GameControllerFactory.java
+++ src/main/java/genius/controller/factory/GameControllerFactory.java
@@ -7,11 +7,15 @@ import genius.model.Mode;
 public class GameControllerFactory {
 
 	public static GameController create(Mode mode) {
-		switch (mode) {
-		case Default:
-			return new DefaultGameController();
-		default:
-			throw new IllegalArgumentException("Could not create game controller for " + mode == null ? "null" : mode.toString() + " mode");
+		if(mode != null) {
+			switch (mode) {
+			case Default:
+				return new DefaultGameController();
+			default:
+				throw new IllegalArgumentException("Could not create game controller for " + mode.toString() + " mode");
+			}
+		} else {
+			throw new IllegalArgumentException("Could not create game controller for null mode");
 		}
 	}
 	
